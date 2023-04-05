796fd016efd812c3d4b076af0b02b00babd71b82
Louis <poes997@gmail.com>
2023-04-05 19:23:09 +0100

    first visualisation script

---
+++ a1.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the CSV file into a pandas DataFrame
+pos_1a_df = pd.read_csv('m_00_01/00_1a_posner_task_2023-04-05_15h00.59.401/00_1a_posner_task_2023-04-05_15h00.59.401.csv')
+pos_1b_df = pd.read_csv('m_00_01/00_1b_posner_task_2023-04-05_16h08.54.140/00_1b_posner_task_2023-04-05_16h08.54.140.csv')
+
+
+# Clean 
+cols = ["block_name", "valid_cue", "key_resp.rt", "key_resp.keys", "cue_dir", "stim_pos"]
+
+a_df = pos_1a_df[cols]
+b_df = pos_1b_df[cols]
+
+a_df = a_df[(a_df['block_name'] == 'trials')]
+b_df = b_df[(b_df['block_name'] == 'trials')]
+
+a_df['cue_dir'] = a_df['cue_dir'].replace({1: 'left', 2: 'right', 3: 'neutral'})
+b_df['cue_dir'] = b_df['cue_dir'].replace({1: 'left', 2: 'right', 3: 'neutral'})
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,5))
+
+a_df['stim_pos'].astype(str)
+
+
+def boxplot_val(my_df, ax, titley):
+    my_df = my_df[~my_df['key_resp.rt'].str.contains(',')]
+    
+    # Split the DataFrame into two based on the 'valid_cue' column
+    valid_df = my_df[my_df['valid_cue']==True]
+    invalid_df = my_df[my_df['valid_cue']==False]
+
+    # Extract the 'key_resp.rt' column and convert it to a list for plotting
+    valid_data = valid_df['key_resp.rt'].str.strip('[]').astype(float).tolist()
+    print("Valid mean: ", np.mean(valid_data))
+    invalid_data = invalid_df['key_resp.rt'].str.strip('[]').astype(float).tolist()
+    print("Invalid mean: ", np.mean(invalid_data))
+    
+    # Plot the boxplots side by side
+    ax.boxplot([valid_data, invalid_data], widths=0.5, sym='o', notch=False)
+    for i, data in enumerate([valid_data, invalid_data]):
+        offset = i + 1
+        y = data
+        x = np.random.normal(offset, 0.04, size=len(y))
+        ax.scatter(x, y, alpha=0.5, edgecolors='none')
+
+    # Add labels and titles
+    ax.set_ylim(0.223, 0.4)
+    ax.set_xticklabels(['Valid', 'Invalid'])
+    ax.set_ylabel('Response Time')
+    ax.set_title(titley)
+    print("....")
+
+def boxplot_stim_pos(my_df, ax, titley):
+    my_df = my_df[my_df['valid_cue']==True]
+    my_df = my_df[~my_df['key_resp.rt'].str.contains(',')]
+    
+    # Split the DataFrame into two based on the 'valid_cue' column
+    left_df = my_df[my_df['stim_pos']=='(-5, -1)']
+    right_df = my_df[my_df['stim_pos']=='(5, -1)']
+
+    # Extract the 'key_resp.rt' column and convert it to a list for plotting
+    left_data = left_df['key_resp.rt'].str.strip('[]').astype(float).tolist()
+    print("Left mean: ", np.mean(left_data))
+    right_data = right_df['key_resp.rt'].str.strip('[]').astype(float).tolist()
+    print("Right mean: ", np.mean(right_data))
+    
+    ax.set_ylim(0.2, 0.4)
+    # Plot the boxplots side by side
+    ax.boxplot([left_data, right_data])
+
+    # Add labels and titles
+    ax.set_xticklabels(['left', 'right'])
+    ax.set_ylabel('Response Time')
+    ax.set_title(titley)
+    print("....")
+
+
+
+#boxplot_stim_pos(a_df, ax1, 'Pre neurofeedback posner')
+#boxplot_stim_pos(b_df, ax2, 'Post neurofeedback posner')
+
+boxplot_val(a_df, ax1, 'Pre neurofeedback posner')
+boxplot_val(b_df, ax2, 'Post neurofeedback posner')
+
+plt.show(block=True)
+
+
+
+
